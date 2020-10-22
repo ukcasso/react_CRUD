a242f61d800b2adc2cbacd8fe66653391f70660e
ukcasso <65535832+ukcasso@users.noreply.github.com>
2020-10-22 23:19:05 +0900

    Add files via upload

---
+++ react_crud/build/precache-manifest.5f8094cc9d321fd336bb10d247dafae4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c00369e5cb5c0613824bbe96bde22cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "466d5f6749e607f00d21",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "c3336a4484db121e59bb",
+    "url": "/static/js/2.d66b5e1e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.d66b5e1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "466d5f6749e607f00d21",
+    "url": "/static/js/main.e8fba187.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
